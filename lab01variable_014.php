e39bcf2850f7d434494a0a23e6cc782a4e84f043
68319010014-del <68319010014@htc.ac.th>
2026-06-15 13:08:47 +0700

    Create lab01variable_014.php

---
+++ lab01variable_014.php
@@ -0,0 +1,17 @@
+<?php
+$name="pin";
+$age=20;
+$height=20000.25;
+$istudent=true;
+
+
+echo gettype($name)."<br>";
+echo gettype($age)."<br>";
+echo gettype($height)."<br>";
+echo gettype($istudent)."<br>";
+
+echo ($name)."<br>";
+echo ($age)."<br>";
+echo($height)."<br>";
+echo ($istudent)."<br>";
+?>
